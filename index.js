ec75538cf506b72d048f120e929bf07e69d69d49
Kayon Marie Douglas <kayonmarie.douglas@mymona.uwi.edu>
2018-11-12 20:57:40 +0000

    exercise 3 - lookup any word with incomplete css

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 // JavaScript File
 window.onload = () => {
     const searchbutton = document.getElementById("search");
+    const textfield = document.getElementById("field");
+    const result = document.getElementById("result");
     let httpRequest = new XMLHttpRequest();
     let url = "https://info2180-lab6-kayonmarie.c9users.io/request.php";
     
     searchbutton.addEventListener('click', search);
     
     function search(){
-        url += '?q=definition';
+        let data = textfield.value;
+        let ext = '?q='+data;
         httpRequest.onreadystatechange = doSomething;
-        httpRequest.open('GET', url, true);
+        httpRequest.open('GET', url+ext, true);
         httpRequest.send();
     }
     
@@ -17,7 +20,7 @@ window.onload = () => {
         if(httpRequest.readyState === XMLHttpRequest.DONE){
             if(httpRequest.status === 200){
                 let response = httpRequest.responseText;
-                alert(response);
+                result.innerHTML = response;
             }
             else{
                 alert('There is a problem with the request');
