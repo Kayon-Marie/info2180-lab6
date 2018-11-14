4c0fe6a2d81eaf2a7a90a6455d7ebdd7f7bcca99
Kayon Marie Douglas <kayonmarie.douglas@mymona.uwi.edu>
2018-11-14 01:23:02 +0000

    exercise 4 - xml data with incomplete css

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ window.onload = () => {
     const searchbutton = document.getElementById("search");
     const textfield = document.getElementById("field");
     const result = document.getElementById("result");
+    const allbutton = document.getElementById("all-searches");
     let httpRequest = new XMLHttpRequest();
     let url = "https://info2180-lab6-kayonmarie.c9users.io/request.php";
     
     searchbutton.addEventListener('click', search);
+    allbutton.addEventListener('click', searchAll);
     
     function search(){
         let data = textfield.value;
@@ -16,11 +18,42 @@ window.onload = () => {
         httpRequest.send();
     }
     
+    function searchAll(){
+        let ext = '?q=&all=true';
+        httpRequest.onreadystatechange = doSomethingXML;
+        httpRequest.open('GET', url+ext, true);
+        httpRequest.send();
+    }
+    
+    function searchResults(responseData){
+        let data = responseData.getElementsByTagName('definition');
+        let list = document.createElement('ol');
+        for(let x = 0; x < data.length; x++){
+            let listElement = document.createElement('li');
+            let html = `<h3>${data[x].getAttribute('name')}</h3><p>${data[x].innerHTML}</p><p>-${data[x].getAttribute('author')}</p>`;
+            listElement.innerHTML = html;
+            list.appendChild(listElement);
+        }
+        return list;
+    }
+    
     function doSomething(){
         if(httpRequest.readyState === XMLHttpRequest.DONE){
             if(httpRequest.status === 200){
                 let response = httpRequest.responseText;
                 result.innerHTML = response;
+            }
+            else{
+                alert('There is a problem with the request');
+            }
+        }
+    }
+    
+    function doSomethingXML(){
+        if(httpRequest.readyState === XMLHttpRequest.DONE){
+            if(httpRequest.status === 200){
+                let response = httpRequest.responseXML;
+                result.appendChild(searchResults(response));
             }
             else{
                 alert('There is a problem with the request');
--- request.php
+++ request.php
@@ -4,6 +4,7 @@
 // respond with a value
 
 $query = $_GET['q'];
+$request = $_GET['all'];
 $definition = [
     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
     "bar" => "A place that sells alcholic beverages",
@@ -14,5 +15,37 @@ $definition = [
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
 
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+if($request == 'true'){
+    $xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+        <entries>
+             <definition name="definition" author="John">
+                A statement of the exact meaning of a word, especially in a dictionary.
+             </definition>
+             <definition name="bar" author="Mary">
+                A place that sells alcholic beverages.
+             </definition>
+             <definition name="ajax" author="Kimberly">
+                Technique which involves the use of javascript and xml (or JSON).
+             </definition>
+             <definition name="html" author="Nathan">
+                The standard markup language for creating web pages and web applications.
+             </definition>
+             <definition name="css" author="Rowan">
+                A style sheet language used for describing the presentation of a document written in a markup language.
+             </definition>
+             <definition name="javascript" author="Kareem">
+                A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+             </definition>
+             <definition name="php" author="Shauna">
+                A server-side scripting language, and a powerful tool for making dynamic and interactive websites.
+             </definition>
+        </entries>';
+        
+    header('Content-Type: text/xml');
+    $xmlOutput = new SimpleXMLElement($xmldata);
+    echo $xmlOutput->asXML();
+}
+else{
+    print "<h3>" . strtoupper($query) . "</h3>";
+    print "<p>" . $definition[$query] . "</p>";
+}    
